5819da9749387e91ea5f9e2d329e5dbfd1f55a3e
Tejaswini Khengare <97454567+Tejaswini179@users.noreply.github.com>
2022-04-08 00:07:42 +0530

    commit

--- Day1(webpack)/webpack-todo-assignment1/src/index.js
+++ Day1(webpack)/webpack-todo-assignment1/src/index.js
@@ -7,12 +7,45 @@
 
  h1.innerHTML="Notes App";
  h1.setAttribute("id","title")
- root.appendChild(h1)
-
-  let div = document.getElementById("input")
-  let input = document.createElement("input").set;
+//  root.appendChild(h1)
 
   
-  div.appendChild(input)
+//   let input = document.createElement("input");
+//   input.setAttribute("id","inputt")
+//   let inputv = document.createElement("input").value;
+// //   let button = document.createElement("button");
+// //   button.setAttribute("click",function(){
+// //       myFunction(inputv)
+// //   })
+// //   button.innerHTML="Add Notes"
+  
+// //   div.append(input,button)
+// function myFunction(){
+//    let h=  document.createElement("h1")
+//    h.innerHTML=`${inputv}`
+// }
 
  
+document.getElementById("myForm").addEventListener("submit",addItem)
+let output = document.getElementById("output")
+let arr = [];
+function addItem(event){
+    event.preventDefault();
+    let name = document.getElementById("name").value;
+   let todoObj = {
+       totoname:name,
+   }
+   arr.push(todoObj);
+   console.log(arr)
+
+   localStorage.setItem("todoList",  JSON.stringify(arr))
+   let h2 = document.createElement("h2")
+   arr.map(function(ele){
+   
+       h2.innerText=ele.totoname
+
+   })
+output.append(h2)
+}
+
+root.append(h1,input)
--- Day1(webpack)/webpack-todo-assignment1/webpack.config.js
+++ Day1(webpack)/webpack-todo-assignment1/webpack.config.js
@@ -11,7 +11,10 @@ module.exports = {
      module: {
        rules: [
          { test: /\.css$/, use: ["style-loader","css-loader"] },
-     
+         {
+          test: /\.worker\.js$/,
+          use: { loader: "worker-loader" },
+        },
        ],
      },
 };
