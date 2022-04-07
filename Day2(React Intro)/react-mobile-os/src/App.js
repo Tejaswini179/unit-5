118f6d918f3accc6d9aa699ebd0d316cb8a65767
Tejaswini Khengare <97454567+Tejaswini179@users.noreply.github.com>
2022-04-07 19:38:24 +0530

    commit

--- Day2(React Intro)/react-mobile-os/src/App.js
+++ Day2(React Intro)/react-mobile-os/src/App.js
@@ -4,20 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div >
+     <ul>
+       <h1>Mobile Operating System</h1>
+       <li> Android</li>
+       <li> Blackberry</li>
+       <li> iPhone</li>
+       <li> Windows Phone</li>
+       </ul>
+     </div>
+
+     <div >
+       <h1>Mobile Manfactures</h1>
+       <ul>
+       <li>Samsung</li>
+       <li>HTC</li>
+       <li>Micromax</li>
+       <ul>
+       <li > Apple</li></ul>
+       </ul>
+     </div>
+      
+      
+       
     </div>
   );
 }
