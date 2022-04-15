017011ed89205d668a48fe91ecfda699926f1573
Tejaswini Khengare <97454567+Tejaswini179@users.noreply.github.com>
2022-04-16 00:49:06 +0530

    week2

--- week2/Day2React - useEffect/timer/src/components/Timer.jsx
+++ week2/Day2React - useEffect/timer/src/components/Timer.jsx
@@ -2,11 +2,33 @@
 import React from "react";
 import "./Timer.css"
 import { useState } from "react";
+import { useEffect } from "react";
 
    function Timer(){
 
     const [second,setSeconds] = useState(0);
-    const [min,setMin] = useState(0)
+    const [min,setMin] = useState(0);
+    var timer;
+    useEffect(()=>{
+      
+        timer=setInterval(()=>{
+            setSeconds(second+1);
+            if(second==59){
+                setMin(min);
+                setSeconds(0)
+            }
+        },1000)
+        return()=>clearInterval(timer)
+    });
+
+    const restart=()=>{
+
+        setSeconds(0);
+        setMin(0)
+    }
+    const stop=()=>{
+        clearInterval(timer)
+    }
     return (
        
         <div className="timer">
@@ -14,7 +36,9 @@ import { useState } from "react";
          <div className="container">
          <div className="timer-container">
              <h1>Timer</h1>
-             <h1>10:30</h1>
+             <h1>{min<10 ? "0"+min:min}:{second<10 ? "0" + second:second}</h1>
+             <button className="restart" onClick={restart}>Restart</button>
+             <button className="stop" onClick={stop}>Stop</button>
          </div>
 
          </div>
